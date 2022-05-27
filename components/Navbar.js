78df5f81d13395a364ed6812f6c32c6038d21e47
unknown <nineplustenisnot21@gmail.com>
2022-05-27 17:43:21 -0400

    made some visual and grammar changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,3 @@
-import styles from "../styles/Navbar.module.css"
 export default function Navbar() {
     return (
         <nav>
@@ -7,14 +6,14 @@ export default function Navbar() {
                     <a href="#about">About Me</a>
                 </li>
                 <li>
+                    <a href="#resume">Resume</a>
+                </li>
+                <li>
                     <a href="#projects">Projects</a>
 
                 </li>
                 <li>
-                    <a href="#contact">Contact</a>    
-                </li>
-                <li>
-                    <a href="#resume">Resume</a>
+                    <a href="#contact">Contact</a>
                 </li>
             </ul>
         </nav>
